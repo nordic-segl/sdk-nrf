93f425aa793fff01cf857ced11930cb8ed48f110
Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
2026-04-07 20:38:35 +0200

    tests: drivers: grtc: grtc_reset: Add mcuboot
    Add mcuboot to grtc_reset test case.
    
    Signed-off-by: Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>

--- tests/drivers/grtc/grtc_reset/pytest/tests.py
+++ tests/drivers/grtc/grtc_reset/pytest/tests.py
@@ -13,6 +13,48 @@ from twister_harness import DeviceAdapter
 
 logger = logging.getLogger(__name__)
 
+
+platform_with_mcuboot_uptime_range_map = {
+    "nrf54h20dk/nrf54h20/cpuapp": range(9, 11),
+    "nrf54l15dk/nrf54l05/cpuapp": range(11, 13),
+    "nrf54l15dk/nrf54l10/cpuapp": range(11, 13),
+    "nrf54l15dk/nrf54l15/cpuapp": range(11, 13),
+    "nrf54lm20dk/nrf54lm20a/cpuapp": range(11, 13),
+    "nrf54lm20dk/nrf54lm20b/cpuapp": range(11, 13),
+    "nrf54ls05dk/nrf54ls05a/cpuapp": range(9, 13),
+    "nrf54ls05dk/nrf54ls05b/cpuapp": range(9, 13),
+    "nrf54lv10dk/nrf54lv10a/cpuapp": range(11, 13),
+}
+
+platform_without_mcuboot_uptime_range_map = {
+    "nrf54h20dk/nrf54h20/cpuapp": range(9, 11),
+    "nrf54l15dk/nrf54l05/cpuapp": range(9, 13),
+    "nrf54l15dk/nrf54l10/cpuapp": range(9, 13),
+    "nrf54l15dk/nrf54l15/cpuapp": range(9, 13),
+    "nrf54lm20dk/nrf54lm20a/cpuapp": range(11, 13),
+    "nrf54lm20dk/nrf54lm20b/cpuapp": range(11, 13),
+    "nrf54ls05dk/nrf54ls05a/cpuapp": range(9, 13),
+    "nrf54ls05dk/nrf54ls05b/cpuapp": range(9, 13),
+    "nrf54lv10dk/nrf54lv10a/cpuapp": range(11, 13),
+}
+
+
+def get_uptime_range(dut: DeviceAdapter, has_mcuboot: bool = False) -> range:
+    platform = dut.device_config.platform
+    at_index = platform.find("@")
+    if at_index != -1:
+        slash_index = platform.find("/", at_index)
+        if slash_index != -1:
+            platform = platform[:at_index] + platform[slash_index:]
+    if has_mcuboot:
+        range_obj = platform_with_mcuboot_uptime_range_map.get(platform)
+    else:
+        range_obj = platform_without_mcuboot_uptime_range_map.get(platform)
+    if range_obj is None:
+        raise ValueError(f"Unsupported platform {platform}, missing uptime range definition")
+    return range_obj
+
+
 def reset_dut(dut: DeviceAdapter, reset_kind: str = "RESET_PIN"):
     cmd = [
         "nrfutil",
@@ -27,37 +69,54 @@ def reset_dut(dut: DeviceAdapter, reset_kind: str = "RESET_PIN"):
     output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=5).decode("utf-8")
     logger.info(output)
 
-def get_cycle_and_uptime_from_logs(dut: DeviceAdapter):
-    lines = dut.readlines_until(regex=r'.*k_uptime_get.*', timeout=5)
+
+def get_cycle_and_uptime_from_logs(dut: DeviceAdapter) -> tuple[int, int]:
+    lines = dut.readlines_until(regex=r".*k_uptime_get.*", timeout=5)
     for line in lines:
-        match_cycle = re.search(r'k_cycle_get_32\s*=\s*(\d+)', line)
+        match_cycle = re.search(r"k_cycle_get_32\s*=\s*(\d+)", line)
         if match_cycle:
-            cycle=(int(match_cycle.group(1)))
-        match_uptime = re.search(r'k_uptime_get\s*=\s*(\d+)', line)
+            cycle = int(match_cycle.group(1))
+        match_uptime = re.search(r"k_uptime_get\s*=\s*(\d+)", line)
         if match_uptime:
-            uptime=(int(match_uptime.group(1)))
+            uptime = int(match_uptime.group(1))
     return cycle, uptime
+
+
+def reset_dut_and_get_cycle_and_uptime(dut: DeviceAdapter, reset_kind: str) -> tuple[int, int]:
+    dut.clear_buffer()
+    reset_dut(dut, reset_kind)
+    return get_cycle_and_uptime_from_logs(dut)
+
 
 def test_grtc_after_reset_system(dut: DeviceAdapter):
     time.sleep(3)
-    dut.clear_buffer()
-    reset_dut(dut, reset_kind="RESET_PIN")
-    cycle_start, uptime_start = get_cycle_and_uptime_from_logs(dut)
-    assert uptime_start in range(8, 30)
-    dut.clear_buffer()
-    reset_dut(dut, reset_kind="RESET_SYSTEM")
-    cycle_after_reset, uptime_after_reset = get_cycle_and_uptime_from_logs(dut)
+    cycle_start, uptime_start = reset_dut_and_get_cycle_and_uptime(dut, reset_kind="RESET_PIN")
+    assert uptime_start in get_uptime_range(dut)
+    cycle_after_reset, uptime_after_reset = reset_dut_and_get_cycle_and_uptime(
+        dut, reset_kind="RESET_SYSTEM"
+    )
     assert cycle_after_reset > cycle_start
-    assert uptime_after_reset in range(8, 30)
+    assert uptime_after_reset in get_uptime_range(dut)
+
 
 def test_grtc_after_reset_pin(dut: DeviceAdapter):
     time.sleep(3)
-    dut.clear_buffer()
-    reset_dut(dut, reset_kind="RESET_PIN")
-    cycle_start, uptime_start = get_cycle_and_uptime_from_logs(dut)
-    assert uptime_start in range(8, 30)
-    dut.clear_buffer()
-    reset_dut(dut, reset_kind="RESET_PIN")
-    cycle_after_reset, uptime_after_reset = get_cycle_and_uptime_from_logs(dut)
+    cycle_start, uptime_start = reset_dut_and_get_cycle_and_uptime(dut, reset_kind="RESET_PIN")
+    assert uptime_start in get_uptime_range(dut)
+    cycle_after_reset, uptime_after_reset = reset_dut_and_get_cycle_and_uptime(
+        dut, reset_kind="RESET_PIN"
+    )
     assert abs(cycle_after_reset - cycle_start) < cycle_start / 2
-    assert uptime_after_reset in range(8, 30)
+    assert uptime_after_reset in get_uptime_range(dut)
+
+
+def test_mcuboot_grtc_uninit(dut: DeviceAdapter):
+    time.sleep(3)
+    _, uptime = reset_dut_and_get_cycle_and_uptime(dut, reset_kind="RESET_PIN")
+    assert uptime in get_uptime_range(dut, has_mcuboot=False)
+
+
+def test_mcuboot_grtc_no_uninit(dut: DeviceAdapter):
+    time.sleep(3)
+    _, uptime = reset_dut_and_get_cycle_and_uptime(dut, reset_kind="RESET_PIN")
+    assert uptime in get_uptime_range(dut, has_mcuboot=True)
